6c88ebf4c27bb1f8d6d067f2bee0e44fd736c37e
Spencer Smitherman <spencer.smitherman@gmail.com>
2014-02-11 22:49:48 -0800

    Added marker but having difficulty adding additional features around it.

--- source/api/public/js/application.js
+++ source/api/public/js/application.js
@@ -2,13 +2,18 @@ $(document).ready(function() {
   $('button').on('click', function() {
   	var mapOptions = {
           center: new google.maps.LatLng(Math.random()*100,Math.random()*100),
+          disableDefaultUI: true,
           zoom: 7
         };
-        var map = new google.maps.Map(document.getElementById("map-canvas"),
-            mapOptions);
-  })
 
-  $('#map-canvas').on('click', function() {
-  	
-});
+    var map = new google.maps.Map(document.getElementById("map-canvas"),
+            mapOptions);
+
+    var marker = new google.maps.Marker({
+	    position: map.getCenter(),
+	    map: map,
+	    title: 'Click to zoom'
+
+  });
+  })
 });
