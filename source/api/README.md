3de138233fe20d73328f59f875b62f4bc143b326
Apprentice <apprentice@dbc15.local>
2014-02-11 23:14:10 -0800

    User can search for github users

--- source/api/README.md
+++ source/api/README.md
@@ -1,3 +1,5 @@
 ## Using the 'X' API
 
-.... write your tutorial here ....
+User puts in a github username
+App returns profile picture, name, location, link to public repos, and the sign up date.
+User may search another github user and it will replace the current displayed github user.
--- source/api/public/js/application.js
+++ source/api/public/js/application.js
@@ -1,7 +1,31 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $('#find_user').on('submit', function(e) {
+    e.preventDefault();
+    $("#input-user").empty();
+    getUser();
+  })
 });
+
+Output = function() {
+  this.template = document.getElementById('template').innerHTML;
+  this.userInfo = null;
+}
+
+Output.prototype.getInfo = function(response) {
+  var userInfo = {
+    name: response.name,
+    location: response.location,
+    repos_url: "https://github.com/" + response.login + "?tab=repositories",
+    created_at: response.created_at,
+    avatar_url: response.avatar_url
+  };
+  return userInfo;
+}
+
+function getUser() {
+  view = new Output();
+  $.get("https://api.github.com/users/" + $('.username').val(), function(response) {
+    view.userInfo = view.getInfo(response);
+    $('#input-user').append(Mustache.render(view.template, view.userInfo));
+  })
+}
