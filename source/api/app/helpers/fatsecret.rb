7a3806cfcece7b2511cae144d8eaf8aa905e7f9e
John Olmsted <johnaolmsted@gmail.com>
2014-02-11 16:23:53 -0800

    Adds description parsing to NutritionGetter

--- source/api/app/helpers/fatsecret.rb
+++ source/api/app/helpers/fatsecret.rb
@@ -1,6 +1,52 @@
+# Class for retrieving and parsing nutrition data using the FatSecret API
 class NutritionGetter
   def self.first_result(food)
-    result = FatSecret.search_food(food)
-    result["foods"]["food"].first
+    result = FatSecret.search_food(food)["foods"]["food"].first
+    food = FoodNutrition.new({
+        name: result["food_name"],
+
+      })
+  end
+
+  def self.parse_description(description)
+    macros = {}
+    macros[:calories] = self.getCals(description)
+    macros[:fat] = self.getFat(description)
+    macros[:carbs] = self.getCarbs(description)
+    macros[:protein] = self.getProtein(description)
+    macros
+  end
+
+  def self.getCals(description)
+    match = description.match(/Calories: (\d+)kcal/)
+    match[1].to_i
+  end
+
+  def self.getFat(description)
+    match = description.match(/Fat: (\d+.\d+)g/)
+    match[1].to_f
+  end
+
+  def self.getCarbs(description)
+    match = description.match(/Carbs: (\d+.\d+)g/)
+    match[1].to_f
+  end
+
+   def self.getProtein(description)
+    match = description.match(/Protein: (\d+.\d+)g/)
+    match[1].to_f
+  end
+end
+
+# Class to model the nutrition data of a food
+class FoodNutrition
+  attr_accessor :calories, :fat, :carbs, :protein
+
+  def initialize(args)
+    @name = args[:name]
+    @calories = args[:calories]
+    @fat = args[:fat]
+    @carbs = args[:carbs]
+    @protein = args[:protein]
   end
 end
