71f33c71266dec1af3b6abd2dcdcffee9eb6551f
Ricardo Rubio <rickarubio@gmail.com>
2014-02-11 21:10:00 -0800

    Adds logic to create DB records to begin building a local cache of price history

--- source/api/app/controllers/index.rb
+++ source/api/app/controllers/index.rb
@@ -4,12 +4,26 @@ get '/' do
   @last_trade_price = @market_data["return"]["markets"]["DOGE"]["lasttradeprice"]
   @last_trade_time = @market_data["return"]["markets"]["DOGE"]["lasttradetime"]
 
+
+  # TODO: This should not be run everytime the page is visited
   params[:name] = @market_data["return"]["markets"].keys.first
   params[:description] = @market_data["return"]["markets"]["DOGE"]["label"]
-  Market.create(params)
+  market = Market.create(params)
 
+  # TODO: We are assuming there is only 1 market right now
+  # the DOGE market (cryptsy market id 132)
+  most_recent_trade = @market_data["return"]["markets"]["DOGE"]["recenttrades"].first
+  market.trades << Trade.create({
+    market_id: Market.first.id,
+    transaction_id: most_recent_trade["id"].to_i,
+    btc_price_per_coin: most_recent_trade["price"],
+    quantity: most_recent_trade["quantity"].to_f,
+    btc_total: most_recent_trade["total"].to_f,
+    executed_at: most_recent_trade["time"]
+    })
 
-
+# btc_price_per_coin_display = sprintf("%0.08f", price)
+# {"id"=>"23102326", "time"=>"2014-02-11 22:51:13", "price"=>"0.00000288", "quantity"=>"24836.87714254", "total"=>"0.07153021"}
 
 
   erb :index
--- source/api/db/migrate/20140211201203_create_trades.rb
+++ source/api/db/migrate/20140211201203_create_trades.rb
@@ -3,9 +3,9 @@ class CreateTrades < ActiveRecord::Migration
     create_table :trades do |t|
       t.belongs_to :market
       t.integer :transaction_id
-      t.decimal :btc_price_per_coin
-      t.decimal :quantity
-      t.decimal :btc_total
+      t.float :btc_price_per_coin
+      t.float :quantity
+      t.float :btc_total
       t.datetime :executed_at
 
       t.timestamps
