a593d79499aab8dd8a7f567e7d55d8b53df69812
Spencer Smitherman <spencer.smitherman@gmail.com>
2014-02-11 20:57:25 -0800

    Generated API Key and can now access map after some true/false troubleshooting.

--- source/api/app/controllers/index.rb
+++ source/api/app/controllers/index.rb
@@ -2,17 +2,3 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
-
-get '/bands' do
-  @band_names = Band.all.map(&:name)
-  erb :bands
-end
-
-post '/bands' do
-  new_band = Band.create!(name: params[:name])
-  new_band.name
-end
-
-get '/info' do
-  Demo.new(self).info
-end
