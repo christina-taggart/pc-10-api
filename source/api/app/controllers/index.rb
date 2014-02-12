79c043db2857168287ac2def22616aa8359dc395
John Olmsted <johnaolmsted@gmail.com>
2014-02-11 18:27:27 -0800

    Adds CSS styling and some design changes

--- source/api/app/controllers/index.rb
+++ source/api/app/controllers/index.rb
@@ -5,5 +5,6 @@ end
 
 get '/foods' do
   @foods = NutritionGetter.first_x_results(params[:food], params[:results_number].to_i)
+  @search_term = params[:food]
   erb :index
 end
--- source/api/app/helpers/fatsecret.rb
+++ source/api/app/helpers/fatsecret.rb
@@ -21,11 +21,18 @@ class NutritionGetter
 
   def self.first_x_results(food, number_of_results)
     results = []
-    number_of_results.times do |index|
-      result = FatSecret.search_food(food)["foods"]["food"][index-1]
-      results << self.buildFoodNutrition(result)
+    if self.food_exists?(food)
+      number_of_results.times do |index|
+        result = FatSecret.search_food(food)["foods"]["food"][index]
+        results << self.buildFoodNutrition(result)
+      end
     end
     results
+  end
+
+  def self.food_exists?(food)
+    result = FatSecret.search_food(food)
+    result["foods"]["food"] ? true : false
   end
 
   def self.buildFoodNutrition(search_result)
@@ -56,16 +63,16 @@ class NutritionGetter
 
   def self.getFat(description)
     match = description.match(/Fat: (\d+.\d+)g/)
-    match[1].to_f
+    match[1].to_f.round(1)
   end
 
   def self.getCarbs(description)
     match = description.match(/Carbs: (\d+.\d+)g/)
-    match[1].to_f
+    match[1].to_f.round(1)
   end
 
    def self.getProtein(description)
     match = description.match(/Protein: (\d+.\d+)g/)
-    match[1].to_f
+    match[1].to_f.round(1)
   end
 end
