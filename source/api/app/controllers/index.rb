adcc3ab95db2689caf33a52237a2f8e485ed46d6
David Goodman <goodman.david.h@gmail.com>
2014-02-11 23:21:09 -0800

    API functional, one query, access taken away

--- source/api/app/controllers/index.rb
+++ source/api/app/controllers/index.rb
@@ -1,18 +1,18 @@
+CLIENT = SoundCloud.new(:client_id => 'ef21557fb8bac5d96f80e45064d414a2')
+
 get '/' do
-  # Look in app/views/index.erb
   erb :index
 end
 
-get '/bands' do
-  @band_names = Band.all.map(&:name)
-  erb :bands
+post '/songs' do 
+	p params
+	@artist = params[0]
+	@tracks = CLIENT.get('/tracks', :limit => 5, :username => params[:artist])
+	erb :songs
 end
 
-post '/bands' do
-  new_band = Band.create!(name: params[:name])
-  new_band.name
-end
 
-get '/info' do
-  Demo.new(self).info
-end
+# client ID:  ef21557fb8bac5d96f80e45064d414a2
+# Secret Key:  27564fd5fb4006639697873dc1393fb7
+# end user autherizatioon: https://soundcloud.com/connect
+# token: https://api.soundcloud.com/oauth2/token
