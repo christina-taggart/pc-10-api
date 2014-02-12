e64f9c171a94dc465bbc69f64e1b8fd44084c9a0
hzla00 <neohzla@gmail.com>
2014-02-11 23:55:26 -0800

    added flickr api

--- source/api/app/controllers/index.rb
+++ source/api/app/controllers/index.rb
@@ -1,18 +1,29 @@
+require 'flickraw'
+
+FlickRaw.api_key= "da3edd0ed10bda12ef1821af5ef42a8d"
+
+FlickRaw.shared_secret= "3b0d25df9725a043"
+
+list   = flickr.photos.getRecent
+
+
+
+random = rand(100)
+
+
 get '/' do
-  # Look in app/views/index.erb
-  erb :index
-end
+	id = list[0].id
+	secret = list[0].secret
+	info = flickr.photos.getInfo :photo_id => id, :secret => secret
+	@photo_url = info.urls[0]["_content"]
 
-get '/bands' do
-  @band_names = Band.all.map(&:name)
-  erb :bands
-end
+	random = rand(100)
+	rand_id = list[random].id
+	rand_secret = list[random].secret
+	rand_info = flickr.photos.getInfo :photo_id => id, :secret => secret
+	@random_user_url = FlickRaw.url_profile(info)
 
-post '/bands' do
-  new_band = Band.create!(name: params[:name])
-  new_band.name
-end
-
-get '/info' do
-  Demo.new(self).info
+	@random_username = flickr.people.getInfo({ :user_id => info["owner"]["nsid"] })["realname"]
+	p @random_username
+	erb :index 
 end
---
+++ source/api/app/controllers/other.rb
@@ -0,0 +1,20 @@
+# require 'flickraw'
+
+# FlickRaw.api_key= "da3edd0ed10bda12ef1821af5ef42a8d"
+
+# FlickRaw.shared_secret= "3b0d25df9725a043"
+
+
+# list   = flickr.photos.getRecent
+
+# id     = list[0].id
+# secret = list[0].secret
+# info = flickr.photos.getInfo :photo_id => id, :secret => secret
+
+
+# p flickr.people.getInfo({ :user_id => info["owner"]["nsid"] })
+
+
+# p FlickRaw.url_profile(info) # => "http://www.flickr.com/people/41650587@N02/"       
+# puts info.dates.taken     # => "2006-07-06 15:16:18"
+
