37df7abf3c210934271ba75e466ba8d1c8bd1870
mh120888 <matthew.alan.higgins@gmail.com>
2014-02-11 17:49:04 -0800

    Can search for a github user and page is modified with the user's name, location, blog, bio, and avatar/photo.  Uses mustache template

--- source/api/public/js/application.js
+++ source/api/public/js/application.js
@@ -2,10 +2,22 @@ $(document).ready(function() {
   $('#stalker').on("submit", function(event) {
     event.preventDefault();
     $.get("https://api.github.com/users/" + $('.username').val(), function(response) {
-      console.log(response);
+      modifyView(response);
     })
   })
 });
+
+modifyView = function(response) {
+  var template = document.getElementById('stalkee').innerHTML;
+  var newInfo = {
+    name: response.name,
+    location: response.location,
+    bio: response.bio,
+    blog: response.blog,
+    avatar_url: response.avatar_url
+  };
+  $('#stalker').append(Mustache.render(template, newInfo));
+}
 
 
 // $.get( "ajax/test.html", function( data ) {
