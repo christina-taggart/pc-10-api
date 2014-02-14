0af372b540b951405458092faa9d8ed8c205c16e
Ricardo Rubio <rickarubio@gmail.com>
2014-02-13 17:02:20 -0800

    Adds unit test for Trade model to test that a transaction id exists and is an integer for a trade object retrieved from the db

--- source/api/spec/trade_spec.rb
+++ source/api/spec/trade_spec.rb
@@ -1,1 +1,9 @@
 require 'spec_helper'
+
+describe 'Trade, an Active Record backed model' do
+  describe 'A Trade object retrieved from the database' do
+    it "should have a transaction id" do
+      Trade.last.transaction_id.should be_a(Integer)
+    end
+  end
+end
