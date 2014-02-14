fbc3a88aa6c4def74abd22df73d500e66f27f8e0
Ricardo Rubio <rickarubio@gmail.com>
2014-02-13 16:40:04 -0800

    adds basic unit testing for Market model

--- source/api/spec/market_spec.rb
+++ source/api/spec/market_spec.rb
@@ -1,7 +1,14 @@
 require 'spec_helper'
 
 describe "Market, an Active Record backed model" do
-  it "should have a name and description" do
-    p Trade.all
+  describe "A Market object retrieved from the DB" do
+    it "should have a name" do
+      Market.last.name.should be_a(String)
+      Market.last.name.should_not be_empty
+    end
+    it "should have a description" do
+      Market.last.name.should be_a(String)
+      Market.last.name.should_not be_empty
+    end
   end
 end
