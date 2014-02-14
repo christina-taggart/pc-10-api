542fb5e4793ffe3827b0fb0757f820f30b63a8b2
Ricardo Rubio <rickarubio@gmail.com>
2014-02-13 19:36:20 -0800

    Began refactoring the controller, adds logic to only ping Cryptsy API once per minute max, moves some logic into Model

--- source/api/app/controllers/index.rb
+++ source/api/app/controllers/index.rb
@@ -1,12 +1,37 @@
 get '/' do
   # Fatty controller but MVP first, refactor second.
   begin
-    cryptsy = Cryptsy::API::Client.new
-    @market_data = cryptsy.marketdata(132) # DOGE is 132 
+    # TODO: this should be moved elsewhere...
+    if Market.all.empty?
+      @market_data = Market.first_run
+    else
+      current_market = Market.first # for now....
+      # TODO create constant instead of magic num 132 for DOGE mkt
+      
+      if (Time.now.hour == current_market.updated_at.min) && (Time.now.day == current_market.updated_at.day)
+        if Time.now.min > current_market.updated_at.min
+          puts "======= UPDATING THE MARKET DATA"
+          current_market.update_latest_market_data!
+        else
+          @market_data = current_market.latest_market_data
+          puts "====================================="
+          puts "did not update market data, only doing that once per min"
+          puts "ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ"
+        end
+      else
+        puts "============ UPDATING THE MARKET DATA"
+        current_market.update_latest_market_data!
+        
+      end
+      @market_data = Market.first.latest_market_data
+    end
+    
     # TODO: This should not be run everytime the page is visited
-    params[:name] = @market_data["return"]["markets"].keys.first
-    params[:description] = @market_data["return"]["markets"]["DOGE"]["label"]
-    market = Market.create(params)  
+    # params[:name] = @market_data["return"]["markets"].keys.first
+    # params[:description] = @market_data["return"]["markets"]["DOGE"]["label"]
+    # params[:cryptsy_market_id] = @market_data["return"]["markets"]["DOGE"]["marketid"]
+    # market = Market.create(params)  
+    market = Market.first # REMOVE ME
     # TODO: We are assuming there is only 1 market right now
     # the DOGE market (cryptsy market id 132)
     most_recent_trade = @market_data["return"]["markets"]["DOGE"]["recenttrades"].first
