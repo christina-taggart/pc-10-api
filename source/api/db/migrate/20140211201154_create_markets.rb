9db972214c70b45127ca7afa6f000580a69e745a
Ricardo Rubio <rickarubio@gmail.com>
2014-02-13 19:39:48 -0800

    Adds cryptsy_market_id and latest_market_data columns to markets table

--- source/api/db/migrate/20140211201154_create_markets.rb
+++ source/api/db/migrate/20140211201154_create_markets.rb
@@ -3,7 +3,9 @@ class CreateMarkets < ActiveRecord::Migration
     create_table :markets do |t|
       t.string :name
       t.string :description
-      
+      t.integer :cryptsy_market_id
+      t.text :latest_market_data
+
       t.timestamps
     end
   end
